5b445bd746530bb0494639da518432ca033ef3b6
Nathan Lima <nathanls.dev@gmail.com>
2024-07-14 13:20:44 -0300

    feat: create component menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import styles from "./Menu.module.css";
+
+export function Menu() {
+  return (
+    <select className={styles.menu}>
+      <option value="">Filter by Region</option>
+      <option value="Africa">Africa</option>
+      <option value="America">America</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="Oceania">Oceania</option>
+    </select>
+  );
+}
