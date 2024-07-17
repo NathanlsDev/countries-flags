27233ce6d1956304cb9f6fe50243773b02a23568
Nathan Lima <nathanls.dev@gmail.com>
2024-07-16 21:21:24 -0300

    feat: link a new component CountryFlag on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Search } from "./components/Search/Search";
 import { Menu } from "./components/Menu";
+import { CountryFlag } from "./components/CountryFlag";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
           <Search />
           <Menu />
         </section>
+        <ul className="flags-list">
+          <CountryFlag />
+        </ul>
       </main>
     </>
   );
