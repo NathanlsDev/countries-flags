15d54609e019ef3013d6b483f3c81aa19582be0d
Nathan Lima <nathanls.dev@gmail.com>
2024-07-20 14:12:23 -0300

    feat: implement handleSelectChange function

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,24 @@
+import PropTypes from "prop-types";
+
 import styles from "./Menu.module.css";
 
-export function Menu() {
+export function Menu({ onRegionChange }) {
+  const handleSelectChange = (event) => {
+    onRegionChange(event.target.value);
+  };
+
   return (
-    <select className={styles.menu}>
+    <select className={styles.menu} onChange={handleSelectChange}>
       <option value="">Filter by Region</option>
       <option value="Africa">Africa</option>
-      <option value="America">America</option>
+      <option value="Americas">Americas</option>
       <option value="Asia">Asia</option>
       <option value="Europe">Europe</option>
       <option value="Oceania">Oceania</option>
     </select>
   );
 }
+
+Menu.propTypes = {
+  onRegionChange: PropTypes.func.isRequired,
+};
