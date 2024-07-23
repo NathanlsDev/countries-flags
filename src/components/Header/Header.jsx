8a95c658a3a1ac7aa9770901fef41a00e38dd1c4
Nathan Lima <nathanls.dev@gmail.com>
2024-07-23 20:54:57 -0300

    feat: implement navigation to home page on header title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import styles from "./Header.module.css";
 import { Button } from "../Button";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <section className={styles.content}>
-        <h1>Where in the world?</h1>
+        <h1 className={styles.title}>
+          <Link to="/">Where in the world?</Link>
+        </h1>
         <Button />
       </section>
     </header>
