0a6d6f0b4b484082257a11c4f8cfd6c218f39404
Nathan Lima <nathanls.dev@gmail.com>
2024-07-28 16:48:49 -0300

    refactor: add a data atribute for observer

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import styles from "./Loader.module.css";
 
 export function Loader() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-id="loader">
       <div className={styles.loader}></div>
     </div>
   );
