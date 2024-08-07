7120c414be001d238a6ef216fe1a087da8f28b83
Nathan Lima <nathanls.dev@gmail.com>
2024-08-07 18:50:28 -0300

    feat: create placeholder component

---
+++ src/components/Placeholder/Placeholder.jsx
@@ -0,0 +1,17 @@
+import imgHolder from "/placeholderImg.svg";
+import styles from "./Placeholder.module.css";
+
+export function Placeholder() {
+  return (
+    <li className={styles.container}>
+      <figure>
+        <img className={styles.image} src={imgHolder} alt="paçoca" />
+      </figure>
+      <section className={styles.section}>
+        <div className={styles.div}></div>
+        <div className={styles.div}></div>
+        <div className={styles.div}></div>
+      </section>
+    </li>
+  );
+}
